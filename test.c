7047f349d764cf4fc45a32525b6518198bd9683b
root <root@DESKTOP-U451335.localdomain>
2025-09-11 17:34:23 +0900

    仮想アドレス実験

--- test.c
+++ test.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+int hoge;
 int main(void){
-    printf("Hello world!");
+    /*2章の学習を開始*/
+    printf("仮想アドレスについて\n");
+    char buf[256];
+
+    printf("&hoge.. %p\n",(void*)&hoge);
+    //入力の規則
+    fgets(buf,sizeof(buf),stdin);
+    sscanf(buf,"%d",&hoge);
+
+    for (;;)
+    {
+        printf("hoge.. %d\n",hoge); //hogeのアドレス
+
+        //enterを押すたび入力された整数の数と同じ値を増やす
+        getchar();
+        hoge++;
+    }
+    
     return 0;
 }
