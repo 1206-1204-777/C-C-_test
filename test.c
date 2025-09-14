8db0524379cfe4c74141f9ae43f629ef2a4d7e40
k <gribeiku@gmail.com>
2025-09-14 14:08:43 +0900

    オーバーフローの実践

--- test.c
+++ test.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-void fanc1(int a, int b){
-    int c,d;
-    //引数のポインタ
-    printf("func1:&a..%p func1:&b..%p\n",(void*)&a,(void*)&b);
-    //関数内のローカル変数のポインタ
-    printf("func1:&c..%p fuc1:&d..%p\n",(void*)&c,(void*)&d);
+void hello(vid){
+    fprintf(stderr,"hello!");
 }
+void fanc(void){
+    void *buf[10];
+    static int i;
+
+for(i = 0; i <100; i++)
+{
+    buf[i] = hello;
+}
+
+}
+
 int main(void){
     /*2章5項の学習を開始*/
-    //動的変数
-   int auto test = 5; //動的変数を定義
-   int static Test = 5;
-   printf("%p\n",(void*)&test);
-   printf("%p\n",(void*)&Test);
-   /*関数を使用した動的変数*/
-   int a,b;
-   //maain関数内のローカル変数
-   printf("main:&a..%p main:&b..%p\n",(void*)&a,(void*)&b);
-   fanc1(1,2);
+    //オーバーフロー
+    int buf[1000];
+    buf[999] = 10;
+    fanc();
     return 0;
 }
