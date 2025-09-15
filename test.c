936d24b39922357b73bd6ef404d58e68ae5686f6
k <gribeiku@gmail.com>
2025-09-15 22:11:56 +0900

    おためしVLA

--- test.c
+++ test.c
@@ -1,53 +1,22 @@
 #include <stdio.h>
-#include<stdarg.h>
-#include<assert.h>
-/*printfライクな関数を作成*/
-void tiny_printf(char *format,...){
-    int i;
-    va_list ap;
+/*C99におけるVLA*/
+void sub(int size1,int size2, int size3){
+    int var1;
+    int array1[size1];
+    int var2;
+    int array2[size2][size3];
+    int var3;
 
-    va_start(ap,format);
-    for(i = 0;format[i] != '\0';i++)
-    switch (format[i])
-    {
-    case 
-    's':
-    printf("%s ",va_arg(ap,char*));
-        break;
-    case
-    'd':
-    printf("%d ",va_arg(ap,int));
-        break;
-    default:
-        assert(0);
-    }
-    va_end(ap);
-    putchar('\n');
-}
-
-static void print_addrs_in_func(int a, int b) {
-    int c, d;
-    // 引数のアドレス
-    printf("func: &a=%p &b=%p\n", (void*)&a, (void*)&b);
-    // 関数内ローカル変数のアドレス
-    printf("func: &c=%p &d=%p\n", (void*)&c, (void*)&d);
+    printf("array1..%p\n",(void*)array1);
+    printf("array2..%p\n",(void*)array2);
+    printf("&var1..%p\n",(void*)&var1);
+    printf("&var2..%p\n",(void*)&var2);
+    printf("&var3..%p\n",(void*)&var3);
 }
 int main(void) {
-    // 動的（自動）変数と静的変数
-    int test = 5;               // automatic storage
-    static int test_static = 5; // static storage duration
-
-    int a, b;
-
-    printf("&test=%p\n", (void*)&test);
-    printf("&test_static=%p\n", (void*)&test_static);
-
-    // main関数内のローカル変数
-    printf("main: &a=%p &b=%p\n", (void*)&a, (void*)&b);
-
-    print_addrs_in_func(1, 2);
-
-    //可変長引数
-    tiny_printf("sdd","result..",5,6);
+    int size1,size2,size3;
+    printf("整数を3つ入力してください\n");
+    scanf("%d%d%d",&size1,&size2,&size3);
+    sub(size1,size2,size3);
     return 0;
 }
