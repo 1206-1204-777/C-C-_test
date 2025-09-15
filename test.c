a83af6ee42b63f29981a73abd4b1f820c6cfe2c2
k <gribeiku@gmail.com>
2025-09-15 09:48:22 +0900

    内容調整

--- test.c
+++ test.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-void fanc1(int a, int b){
-    int c,d;
-    //引数のポインタ
-    printf("func1:&a..%p func1:&b..%p\n",(void*)&a,(void*)&b);
-    //関数内のローカル変数のポインタ
-    printf("func1:&c..%p fuc1:&d..%p\n",(void*)&c,(void*)&d);
+
+static void print_addrs_in_func(int a, int b) {
+    int c, d;
+    // 引数のアドレス
+    printf("func: &a=%p &b=%p\n", (void*)&a, (void*)&b);
+    // 関数内ローカル変数のアドレス
+    printf("func: &c=%p &d=%p\n", (void*)&c, (void*)&d);
 }
-int main(void){
-    /*2章5項の学習を開始*/
-    //動的変数
-   int auto test = 5; //動的変数を定義
-   int static Test = 5;
-   printf("%p\n",(void*)&test);
-   printf("%p\n",(void*)&Test);
-   /*関数を使用した動的変数*/
-   int a,b;
-   //maain関数内のローカル変数
-   printf("main:&a..%p main:&b..%p\n",(void*)&a,(void*)&b);
-   fanc1(1,2);
+int main(void) {
+    // 動的（自動）変数と静的変数
+    int test = 5;               // automatic storage
+    static int test_static = 5; // static storage duration
+
+    int a, b;
+
+    printf("&test=%p\n", (void*)&test);
+    printf("&test_static=%p\n", (void*)&test_static);
+
+    // main関数内のローカル変数
+    printf("main: &a=%p &b=%p\n", (void*)&a, (void*)&b);
+
+    print_addrs_in_func(1, 2);
     return 0;
 }
