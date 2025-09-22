e0d19b5c3c15e23c6c2becb8f15baaa0e2454664
k <gribeiku@gmail.com>
2025-09-22 23:28:49 +0900

    アライメントの学習

--- test.c
+++ test.c
@@ -1,53 +1,29 @@
 #include <stdio.h>
-#include<stdarg.h>
-#include<assert.h>
-/*printfライクな関数を作成*/
-void tiny_printf(char *format,...){
-    int i;
-    va_list ap;
-
-    va_start(ap,format);
-    for(i = 0;format[i] != '\0';i++)
-    switch (format[i])
-    {
-    case 
-    's':
-    printf("%s ",va_arg(ap,char*));
-        break;
-    case
-    'd':
-    printf("%d ",va_arg(ap,int));
-        break;
-    default:
-        assert(0);
-    }
-    va_end(ap);
-    putchar('\n');
-}
-
-static void print_addrs_in_func(int a, int b) {
-    int c, d;
-    // 引数のアドレス
-    printf("func: &a=%p &b=%p\n", (void*)&a, (void*)&b);
-    // 関数内ローカル変数のアドレス
-    printf("func: &c=%p &d=%p\n", (void*)&c, (void*)&d);
-}
+/*構造体定義*/
+typedef struct Hoge
+{
+    char    char1;
+    int     int1;
+    char    char2;
+    double  double1;
+    char    char3;
+}Hoge;
+/*構造体ごとのポインタを表示*/
 int main(void) {
-    // 動的（自動）変数と静的変数
-    int test = 5;               // automatic storage
-    static int test_static = 5; // static storage duration
+    Hoge    hoge;
+    //構造体(Hoge)の大きさ
+    printf("hoge size.. %d\n",(int)sizeof(Hoge));
 
-    int a, b;
+    //構造体とその中身のポインタ
+    printf("hoge.. %p\n",(void*)&hoge);
+    printf("char1.. %p\n",(void*)&hoge.char1);
+    printf("int1.. %p\n",(void*)&hoge.int1);
+    printf("char2.. %p\n",(void*)&hoge.char2);
+    printf("double1.. %p\n",(void*)&hoge.double1);
+    printf("char3.. %p\n",(void*)&hoge.char3);
 
-    printf("&test=%p\n", (void*)&test);
-    printf("&test_static=%p\n", (void*)&test_static);
-
-    // main関数内のローカル変数
-    printf("main: &a=%p &b=%p\n", (void*)&a, (void*)&b);
-
-    print_addrs_in_func(1, 2);
-
-    //可変長引数
-    tiny_printf("sdd","result..",5,6);
+    hoge.int1 = 10;
+    printf("%i\n",hoge.int1);
+    
     return 0;
 }
