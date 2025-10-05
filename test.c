e96e30888ca4b57174ed3596cba6f07708ea43de
k <xingyanjintian@gmail.com>
2025-10-05 18:10:10 +0900

    データ型についての学習

--- test.c
+++ test.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 int main(void){
   /*Cのデータ型についての学習*/
-    return 0; 
+  int x = 10;
+  int *p = &x;
+  printf("p:%zu\n",sizeof(p));
+  printf("int:%zu\n",sizeof(x));
+  printf("x..%i\n",x);
+  printf("*p..%p\n",&p);
+
+  int xx = x + 1;
+  int *pp = &xx;
+  printf("xx..%i\n",xx);
+  printf("*pp..%p\n",&pp);
+
+  /*配列について*/
+  int hoge[2][3];
+  printf("hoge[2]..%i\n",(*hoge)[3]);
+  printf("hoge[2]..%i\n",(*hoge)[2]);
+  /*配列の計算 */
+  printf("size.. %d\n",(int)sizeof(int(*[5])(double)));
+
+  
+  return 0; 
 }
