4d930bb29fba79804965a1e67bd7c8e7ce2e72a1
k <xingyanjintian@gmail.com>
2025-10-07 14:09:26 +0900

    式についての学習

--- test.c
+++ test.c
@@ -1,47 +1,18 @@
 #include <stdio.h>
+int sum(int x,int y){
+  return printf("x + y = %i\n",x + y);
+}
+
 int main(void){
-  /*Cのデータ型についての学習*/
-  int x = 10;
-  int *p = &x;
-  printf("p:%zu\n",sizeof(p));
-  printf("int:%zu\n",sizeof(x));
-  printf("x..%i\n",x);
-  printf("*p..%p\n",&p);
-
-  int xx = x + 1;
-  int *pp = &xx;
-  printf("xx..%i\n",xx);
-  printf("*pp..%p\n",&pp);
-
-  /*配列について*/
-  int hoge[2][3];
-
-  printf("hoge[2]..%i\n",(*hoge)[2]);
-  /*配列の計算 */
-  printf("size.. %d\n",(int)sizeof(int(*[5])(double)));
-
-  /*基本型のバイト数*/
-  printf("char..%d\n",(int)sizeof(char));
-  printf("int..%d\n",(int)sizeof(int));
-  printf("float..%d\n",(int)sizeof(float));
-  printf("double..%d\n",(int)sizeof(double));
-
-    //ポインタのテスト
-  int* p1 = NULL;//int型へのポインタ変数
-  int i;
-  i= 10;
-  //iのアドレスをpに代入
-  p1 = &i;
-
-  //pの値
-  printf("p = %p\n",p1);
-  //iの値
-  printf("&i = %p\n",&i);
-
-  /*配列型*/
-  int array[5] = {10, 20, 30, 40, 50}; 
-  printf("*array = %d\n", *array); //　10
-  printf("array[3] = %d\n", array[3]); // 40 
-  printf("*(array + 3) = %d\n", *(array + 3)); //　3つ目の要素に対してポインタ経由でアクセス
+  /*Cの式についての学習*/
+  //一次式
+  int hoge_list[10];
+  int hoge = 10;
+  int x = 5;
+  printf("hoge + x = %i\n",(hoge + x) / 5);
+  sum(10,5);
+  printf("Hello!\n");
+  printf("hoge_list..%p\n",hoge_list);
+  printf("hoge_list..%p\n",(void*)&hoge_list[0]);
   return 0; 
 }
