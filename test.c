bdb0abcd1ad5118e5043978a6a2deea654ae5c90
k <xingyanjintian@gmail.com>
2025-10-14 18:00:50 +0900

    コメント修正

--- test.c
+++ test.c
@@ -9,7 +9,7 @@ int main(void){
    /*引数にconstを使用した関数*/
    char *test_src(char *dest,const char *src){ //char型へのポインタを返す関数testを宣言
     src = NULL; //constで修飾したポインタ型変数"src"に値を再代入するが、問題なく実行・コンパイルされる
-   //*src = 'a'; //この場合は"変数aの指す先(値そのもの)を変えることになるためコンパイルエラーになる" エラーメッセージ：assignment of read-only location ‘*src’
+   //*src = 'a'; //この場合は"変数srcの指す先(値そのもの)を変えることになるためコンパイルエラーになる" エラーメッセージ：assignment of read-only location ‘*src’
    }
    //変数そのものを"読み出し専用"にする場合
    char *test_src2(char *dest, char * const src){
