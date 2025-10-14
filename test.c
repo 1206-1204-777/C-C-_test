951ae1e7532a492b8d2284081f65972c162710ae
k <xingyanjintian@gmail.com>
2025-10-14 13:07:46 +0900

    エラー実験

--- test.c
+++ test.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main(void){
    /*const修飾子*/
-   const int x = 10;
-   printf("x..%d\n",x);
-   int y = x + 10;
+   const int x = 10; //値の変更が不可な変数"x"を宣言
+   printf("x..%d\n",x); //xの値を出力
+   x = 20; /*変更できないためコンパイルエラーになる(エラーメッセージ：式は変更可能な左辺値である必要があります)*/
+   int y = x + 10; 
    printf("y..%d\n",y); 
     return 0; 
 }
