7140a8cc0bf15d7c35a6c983b1484c9913dbd1f4
k <xingyanjintian@gmail.com>
2025-10-15 20:12:16 +0900

    構造体と関数にtypedefを使用

--- test.c
+++ test.c
@@ -23,5 +23,20 @@ int main(void){
    typedef char String; //typedefを使ってchar型にStringという別名をつける
    String X[6] = "hello"; //Stringの別名を使って配列Xに終端文字(\0)を含む文字列を格納
    printf("x..%s\n",X); //結果x..helloと出力される
+   /*typedefを使用した構造体*/
+   typedef struct {
+    int x;
+    int y;
+   }Point; //Pointという名前の構造体を定義
+   Point p1 = {10,20}; //Point型の変数p1を宣言し、xに10、yに20を代入
+   printf("p1..x:%d y:%d\n",p1.x,p1.y); //結果：p1..x:10 y:20と出力される
+   /*typedefを使用した関数ポインタ*/
+   typedef int (*FuncPtr)(int,int); //戻り値がint型、引数が2つのint型の関数ポインタにFuncPtrという別名をつける
+   int add(int a,int b){ //2つのint型引数を受け取り、その和を返す関数addを定義
+    return a + b; //引数aとbの和を返す
+   }
+   FuncPtr fptr = add; //関数ポインタfptrに関数addのアドレスを代入
+   int result = fptr(5,3); //fptrを使って関数addを呼び出し、結果をresultに格納
+   printf("result..%d\n",result); //結果：result..8と出力される
     return 0; 
 }
