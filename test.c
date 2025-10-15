81fddad652e518b3a63beefa50cead7be0529687
k <xingyanjintian@gmail.com>
2025-10-15 20:51:19 +0900

    テストコードの一部修正

--- test.c
+++ test.c
@@ -7,17 +7,17 @@ int main(void){
    int y = x + 10; // xの値を使った新しい変数"y"を作成。constは"読み取り専用"なだけなので中身の値は使える
    printf("y..%d\n",y); //結果：y..20
    /*引数にconstを使用した関数*/
-   char *test_src(char *dest,const char *src){ //char型へのポインタを返す関数test_srcを宣言
+   void *test_src(char *dest,const char *src){ //char型へのポインタを返す関数test_srcを宣言
     src = NULL; //constで修飾したポインタ型変数"src"に値を再代入するが、問題なく実行・コンパイルされる
    //*src = 'a'; //この場合は"変数srcの指す先(値そのもの)を変えることになるためコンパイルエラーになる" エラーメッセージ：assignment of read-only location ‘*src’
    }
    //変数そのものを"読み出し専用"にする場合
-   char *test_src2(char *dest, char * const src){
+   void *test_src2(char *dest, char * const src){
     //src = NULL; //変数そのものが読み出し専用なので、再代入ができずコンパイルエラーになる エラーメッセージ：assignment of read-only parameter ‘src’
     *src = 'a'; //新しい値をポインタ変数が指す先の変更は可能(再代入は不可)
    }
    //変数と変数に格納された値の両方をconstにしたい場合
-   char *test_src3(const char * const src);
+   void *test_src3(const char * const src);
 
    /*typedef*/
    typedef char String; //typedefを使ってchar型にStringという別名をつける
