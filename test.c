ba41164a9f87a72864d95901dc190a19b6e1428a
k <xingyanjintian@gmail.com>
2025-10-21 20:44:55 +0900

    ポインタを使い配列の要素を渡す

--- test.c
+++ test.c
@@ -5,6 +5,14 @@ void func(int *a,double *b){ //引数にポインタを指定
    //渡す値。戻り値ではなく引数を使う
    *a = 5;
    *b = 10.5;
+}
+/*配列の先頭要素のポインタを引数に指定*/
+void func2(int *array,int size){
+ int i;
+ for(i = 0; i < size; i++)
+ {
+   printf("array[%d]..%d\n",i, array[i]);
+ }
    
 }
 int main(void){
@@ -13,5 +21,9 @@ int main(void){
    double b;
    func(&a,&b); //空の変数を引数に渡す
    printf("a..%d b..%f\n",a,b);
-    return 0; 
+
+   int array[] = {2,2,3,4,5,6};
+   func2(array,sizeof(array) / sizeof(array[0]));
+   printf("%lu\n",sizeof(array)/sizeof(array[0]));//配列のサイズ
+   return 0; 
 }
